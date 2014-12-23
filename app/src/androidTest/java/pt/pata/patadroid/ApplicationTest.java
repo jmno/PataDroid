4d6d920b567379552ce733398f95753f11824b8c
Nicolau <crbikers@gmail.com>
2014-12-23 01:51:03 +0000

    Inicio

---
+++ app/src/androidTest/java/pt/pata/patadroid/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pt.pata.patadroid;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
